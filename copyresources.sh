cef6557ca899d302d473f5fe07b90da90e12bb39
Rob Wilds <robwilds@me.com>
2025-01-06 07:40:56 -0500

    initial commit

---
+++ copyresources.sh
@@ -0,0 +1,2 @@
+docker cp  alfrescodemocontainer-alfresco-1:/usr/local/tomcat/alf_data ./data/services/content
+docker cp  alfrescodemocontainer-postgres-1:/var/lib/postgresql/data ./data/services/postgres/ 
